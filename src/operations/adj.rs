2f621eebc324411fc349745b4783612d120132d7
PFiS1737 <pfis1737@gmail.com>
2025-06-05 09:13:46 +0800

    test: add unit test for adjugate

--- src/operations/adj.rs
+++ src/operations/adj.rs
@@ -27,3 +27,27 @@ impl Matrix {
         Ok(self.get_cofactor_matrix()?.transpose())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{matrix, Matrix, MatrixElement};
+
+    #[test]
+    fn adj() {
+        let m1 = matrix![
+            2, 5, 7;
+            -3, 6, -2;
+            -8, 1.5, 9;
+        ];
+        let m2 = matrix![
+            1, 9, -2.5;
+            -15, 7, -3;
+            -3.5, 7.5, 10;
+        ];
+
+        assert!((m1.clone() * m2.clone())
+            .adj()
+            .unwrap()
+            .epsilon_equals(&(m2.adj().unwrap() * m1.adj().unwrap())))
+    }
+}
